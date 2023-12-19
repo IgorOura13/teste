a89b662a2cea236285fbdc36de69d2f13f2829de
IgorOura13 <87547315+IgorOura13@users.noreply.github.com>
2023-12-18 21:06:51 -0300

    Update pg1_guide1.py

--- page_1/pg1_guide1.py
+++ page_1/pg1_guide1.py
@@ -117,51 +117,6 @@ def run_guide1():
 
             # Corresponding variables with their numbers
 
-            x1 = actives[x1]
-            x9 = tests[x9]
+
             
-            def float_values():
-                return [float(x) for x in [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15]]
-
-            submitted = st.form_submit_button("Predict")
-            
-            if submitted:
-
-                float_inputs = float_values()
-                columns = list(dataframe().keys())
-                prediction_df = dict(zip(columns, float_inputs))
-                prediction_df = pd.DataFrame(prediction_df, index = [0])
-                prediction_df = one_hot_for_prediction(prediction_df)
-
-                clf = model()
-                float_prediction = clf.predict(prediction_df)
-                prediction = results[float_prediction[0]]
-
-                if prediction == 'Disapproved':
-                    st.error(prediction)
-                elif prediction == 'Approved':
-                    st.success(prediction)
-
-                st.session_state['predict_pg1_guide1'] = True
-
-        if st.session_state['predict_pg1_guide1']:
-
-            # Explainer algorithm
-
-            warnings.filterwarnings("ignore")
-
-            st.write("Variables influence in decision classification:")
-
-            explainer = shap.TreeExplainer(clf)
-            shap_values = explainer.shap_values(prediction_df)
-
-            prediction_df.columns = ind
-
-            if prediction == 'Disapproved':
-                st_shap(shap.force_plot(explainer.expected_value[0], shap_values[1][0,:], prediction_df.iloc[0,:]), height = 150)
-            elif prediction == 'Approved':
-                st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1][0,:], prediction_df.iloc[0,:]), height = 150)
-
-    except ValueError:
-        st.warning("Warning: please fill in all the gaps")
-        st.warning("Warning: please enter numbers only")
+          
