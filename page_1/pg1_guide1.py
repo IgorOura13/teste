7134b643a8d053f8e657eaf360c7232b7b73d7fa
nicolekreischer <152092165+nicolekreischer@users.noreply.github.com>
2023-12-22 20:27:36 -0300

    Update pg1_guide1.py

--- page_1/pg1_guide1.py
+++ page_1/pg1_guide1.py
@@ -15,28 +15,6 @@ results = {
     1: 'Approved',
 }
 
-actives = {
-    'None': 0,
-    'Sodium Alkyl Benzene Sulfonate': 1,
-    'Benzalkonium Chloride': 2,
-    'Dialkyloxyethyl Hydroxyethyl Methyl Ammonium Methyl Sulphate': 3,
-    'Hydrogen Peroxide': 4,
-    'Sodium hypochlorite': 5,
-    'Ethoxylated Alcohol': 6,
-    'Lactic acid': 7,
-    'Alkyl Dimethyl Benzyl Ammonium Chloride': 8
-}
-
-tests = {
-    'EN1276': 1,
-    'ASTM2274': 2,
-    'EN1040': 3,
-    'Inhibition halo': 4,
-    'OECD202': 5,
-    'EN13697': 6,
-    'EN14561': 7
-}
-
 # Columns names for plots
 
 ind = ['Corrente Agitador 1601',
@@ -44,23 +22,24 @@ ind = ['Corrente Agitador 1601',
     'Corrente Agitador 1603',
     'Corrente Agitador 1604',
     'Corrente Agitador 1605',
+    'Corrente BB 1006',
     'Corrente Bomba 1601',
     'Corrente Bomba 1602',
-    'Corrente BB 1006',
     'Potência Agitador 1605',
     'Potência Bomba 1006',
-    'Vazão Pasta',
+    'Vazão Pasta CV',
     'Pressão CZ 1603',
     'Pressão CZ 1605',
+    'Vazão Pasta PV',
     'Temperatura Final',
     'Temperatura Holding 1633',
     'Temperatura Holding 1634',
-    'Temperatura 1 Corpo',
     'Temperatura 3 Corpo',
+    'Temperatura 1 Corpo',
     'Temperatura Amostra',
     'Dosagem Açúcar',
-    'Dosage Óleo',
-    'Dosagem Água',
+    'Dosage Água',
+    'Dosagem Óleo',
     'Dosagem Vinagre'
       ]
 def run_guide1():
@@ -76,42 +55,67 @@ def run_guide1():
 
             var1, var2 = st.columns(2)
 
-            x1 = var1.selectbox("Active:", actives.keys())
-            x2 = var2.text_input("Corrente Agitador:")
+            x1 = var1.text_input("Corrente Agitador 1601:")
+            x2 = var2.text_input("Corrente Agitador 1602:")
 
             var3, var4 = st.columns(2)
 
-            x3 = var3.text_input("Corrente Bomba:")
-            x4 = var4.text_input("Corrente BB:")
+            x3 = var3.text_input("Corrente Agitador 1603:")
+            x4 = var4.text_input("Corrente Agitador 1604:")
 
             var5, var6 = st.columns(2)
 
-            x5 = var5.text_input("Potência Bomba:")
-            x6 = var6.text_input("Potência Agitador:")
+            x5 = var5.text_input("Corrente Agitador 1605:")
+            x6 = var6.text_input("Corrente BB 1006:")
 
             var7, var8 = st.columns(2)
 
-            x7 = var7.text_input("Temperatura Holding:")
-            x8 = var8.text_input("Pressão CZ:", help = "Unit: %")
+            x7 = var7.text_input("Corrente Bomba 1601:")
+            x8 = var8.text_input("Corrente Bomba 1602:")
 
             var9, var10 = st.columns(2)
 
-            x9 = var9.selectbox("Dosagem Açúcar:", tests.keys())
-            x10 = var10.text_input("Dosagem Vinagre:", help = "Unit: %")
+            x9 = var9.text_input("Potência Agitador 1605:")
+            x10 = var10.text_input("Potência Bomba 1006:")
 
             var11, var12 = st.columns(2)
 
-            x11 = var11.text_input("Dosagem Água:", help = "Unit: min")
-            x12 = var12.text_input("Dosagem Óleo:", help = "Unit: ºC")
+            x11 = var11.text_input("Vazão Pasta CV:")
+            x12 = var12.text_input("Pressão CZ 1603:")
 
-            # Corresponding variables with their numbers
+            var13, var14 = st.columns(2)
 
-            x1 = actives[x1]
-            x9 = tests[x9]
+            x13 = var13.text_input("Pressão CZ 1605:")
+            x14 = var14.text_input("Vazão Pasta PV':")
+
+            var15, var16 = st.columns(2)
+
+            x15 = var15.text_input("Temperatura Final:")
+            x16 = var16.text_input("Temperatura Holding 1633:")
+
+            var17, var18 = st.columns(2)
+
+            x17 = var17.text_input("Temperatura Holding 1634:")
+            x18 = var18.text_input("Temperatura 3 Corpo:")
+
+            var19, var20 = st.columns(2)
+
+            x19 = var19.text_input("Temperatura 1 Corpo:")
+            x20 = var20.text_input("Temperatura Amostra:")
+
+            var21, var22 = st.columns(2)
+
+            x21 = var21.text_input("Dosagem Açúcar:")
+            x22 = var22.text_input("Dosagem Água:")
+
+            var23, var24 = st.columns(2)
+
+            x23 = var23.text_input("Dosagem Óleo:")
+            x24 = var24.text_input("Dosagem Vinagre:")
             
             submitted = st.form_submit_button("Predict")
             def float_values():
-                return [float(x) for x in [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12]]
+                return [float(x) for x in [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24]]
 
 
             if submitted:
