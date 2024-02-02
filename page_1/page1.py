dcbd6eefa526a1a35f0f0313e8e6b4d56da9a2b8
IgorOura13 <87547315+IgorOura13@users.noreply.github.com>
2024-02-02 10:36:22 -0300

    Update page1.py

--- page_1/page1.py
+++ page_1/page1.py
@@ -3,23 +3,22 @@ import hydralit_components as hc
 
 # Internal libraries
 from page_1.pg1_guide2 import run_guide2
+from page_1.pg1_guide1 import run_guide1
 
 def run_page1():
 
     try:
 
         guides = [
-            {'icon': "bi bi-pencil-square", 'label':"Add data by hand"},
-            {'icon':"bi bi-file-earmark-arrow-up",'label':"Upload spreadsheet"},
+            {'icon':"bi bi-file-earmark-arrow-up",'label':"Upload spreadsheet to make a prediction"},
         ]
         over_theme = {'txc_inactive': '#262730','menu_background':'#F0F2F6','txc_active':'white','option_active':'#4073ca'}
         guide = hc.option_bar(option_definition = guides,override_theme = over_theme, horizontal_orientation=True, key="page1")
 
         
+        
 
-        if guide == "Upload spreadsheet":
-
-            run_guide2()
+        run_guide2()
     
     except UnboundLocalError:
         pass
