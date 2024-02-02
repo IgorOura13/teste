81e7edd0c99ecb05c0e5c5dd1df59a3413a57e0a
IgorOura13 <87547315+IgorOura13@users.noreply.github.com>
2024-02-02 10:35:46 -0300

    Update page2.py

--- page_2/page2.py
+++ page_2/page2.py
@@ -7,12 +7,10 @@ from page_2.pg2_guide2 import run_guide2
 def run_page2():
 
     guides_auto = [
-            {'icon':"bi bi-file-earmark-arrow-up",'label':"Upload spreadsheet"}
+            {'icon':"bi bi-file-earmark-arrow-up",'label':"Upload spreadsheet to add data"}
         ]    
     over_theme_auto = {'txc_inactive': '#262730','menu_background':'#F0F2F6','txc_active':'white','option_active':'#4073ca'}
     guide_auto = hc.option_bar(option_definition = guides_auto,override_theme = over_theme_auto, horizontal_orientation=True, key="page2")
 
 
-    if guide_auto == "Upload spreadsheet":
-
-        run_guide2()
+    run_guide2()
