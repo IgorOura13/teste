b347f353c2574c6d8351b001203c045185528ff7
IgorOura13 <87547315+IgorOura13@users.noreply.github.com>
2024-02-06 20:30:00 -0300

    Update format_data.py

--- format_data.py
+++ format_data.py
@@ -83,8 +83,8 @@ def run_format_data(dados_processos, dados_viscosidade):
     }
     dados_viscosidade = dados_viscosidade.rename(columns = mapa)
     dados_viscosidade['TQ SUSPENSÃO'] = dados_viscosidade['TQ SUSPENSÃO'].apply(lambda x: ''.join(re.findall(r'\d+', str(x))))
-
-    dados_viscosidade = dados_viscosidade.drop(["VEL_1_RPM", "COZEDOR", "ASPECTO", " ID"], axis=1)
+    if ["VEL_1_RPM", "COZEDOR", "ASPECTO", " ID"] in dados_viscosidade.columns:
+        dados_viscosidade = dados_viscosidade.drop(["VEL_1_RPM", "COZEDOR", "ASPECTO", " ID"], axis=1)
 
     pd.set_option('display.max_columns', None)
 
@@ -106,7 +106,8 @@ def run_format_data(dados_processos, dados_viscosidade):
     dados_processos.iloc[0:, 1:]=scaler.fit_transform(dados_processos.iloc[0:, 1:].to_numpy())
     dados_viscosidade['TQ SUSPENSÃO'] = pd.to_numeric(dados_viscosidade['TQ SUSPENSÃO'], errors='coerce')
     dados_viscosidade[['TQ SUSPENSÃO',' TEMP AMOSTRA C°']]=scaler.fit_transform(dados_viscosidade[['TQ SUSPENSÃO',' TEMP AMOSTRA C°']].to_numpy())
-    dados_viscosidade[[' VEL_5_RPM']]=scaler.fit_transform(dados_viscosidade[[' VEL_5_RPM']].to_numpy())
+    if ' VEL_5_RPM' in dados_viscosidade.columns:
+        dados_viscosidade[[' VEL_5_RPM']]=scaler.fit_transform(dados_viscosidade[[' VEL_5_RPM']].to_numpy())
 
     # Adicione 4 minutos a cada valor na coluna 'DataHora'
     dados_processos['Data'] = dados_processos['Data'] + pd.Timedelta(minutes=4)
