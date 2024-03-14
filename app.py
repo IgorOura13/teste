d7400926baf1e194f30370f51c6527a3973fe5fe
IgorOura13 <87547315+IgorOura13@users.noreply.github.com>
2024-03-14 17:20:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # External libraries
 import streamlit as st
 from streamlit_option_menu import option_menu
-import
+
 # Internal libraries
 from machine_learning_algorithm import dataframe, visc_path, proc_path
 from page_1.page1 import run_page1
